810d5d40977579f6f17c3e0cb1348bf4b4ae5028
Romi5304 <romimohapatra100@gmail.com>
2024-11-01 18:54:30 +0530

    task 5 added

---
+++ Frontend/javascript/class 5/array.js
@@ -0,0 +1,9 @@
+//concat:
+let fruits=["apple","mango"];
+let addarray=fruits.concat(["stawberry","dragon-fruit"]);
+console.log(addarray);
+
+// const animal=['ant','bison','camel','duck','elephant']
+// const slice=animal.slice(2,4);
+// console.slice(2,4);
+// console.log(slice);
---
+++ Frontend/javascript/class1/Index.js
@@ -0,0 +1,11 @@
+// var name = "Romi";
+
+// let age = 24;
+
+// const city = "Bhubaneswar";
+
+console.log(address);
+
+var address = "Bhubaneswar";
+
+console.log(address);
